03a6a774f28f21304ec0403985ab02025209a81d
FaridMoghadam <sabahi.farid@gmail.com>
2022-11-09 23:54:11 +0330

    V2-Fix some Error.

--- concatenatedTables.py
+++ concatenatedTables.py
@@ -1,30 +1,29 @@
-import pandas as pd 
 import os
 import glob
 import pandas as pd
 
 class Preprocess:
-   def listdir_labels(self):
-      #-- This section is for identifying all the files in this folder.
+   #-- This section is for identifying all the files in this folder.
+   def identifying_Files(self):
       try:
         entries = os.listdir()
       except:
         print("Something went wrong !")
       else:
         print("I found some files.")
-        Preprocess().create_Table()
+        Preprocess().create_file()
 
+   #-- This section is for Splitting Excel files and merging them all
    def Concatenate_Table(self):
-      #-- Splitting Excel files and merging them all
       try:
         all_files = glob.glob("*.xlsx")
         df = pd.concat((pd.read_excel(f) for f in all_files))
         return df
       except:
         print("I cannot Concatenate !")
-      
-   def create_Table(self): 
-      #-- Create a new file that contains all Excel data
+
+   #-- Create a new file that contains all Excel data 
+   def create_file(self): 
       try:
          df = Preprocess().Concatenate_Table()
          df.to_excel('Final.xlsx', index=True, header=True)
@@ -34,4 +33,4 @@ class Preprocess:
          print("Created New File Done.")
 
 if __name__ == '__main__':
-    Preprocess().listdir_labels()
+    Preprocess().identifying_Files()
