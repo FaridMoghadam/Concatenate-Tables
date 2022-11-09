c1eea83f36e57e1894908221453c51ec586b4d7e
FaridMoghadam <sabahi.farid@gmail.com>
2022-11-09 23:09:38 +0330

    Version 1 - no bugs

--- concatenatedTables.py
+++ concatenatedTables.py
@@ -3,15 +3,35 @@ import os
 import glob
 import pandas as pd
 
+class Preprocess:
+   def listdir_labels(self):
+      #-- This section is for identifying all the files in this folder.
+      try:
+        entries = os.listdir()
+      except:
+        print("Something went wrong !")
+      else:
+        print("I found some files.")
+        Preprocess().create_Table()
 
-entries = os.listdir('/')
-for entry in entries:
-    print(entry)
+   def Concatenate_Table(self):
+      #-- Splitting Excel files and merging them all
+      try:
+        all_files = glob.glob("*.xlsx")
+        df = pd.concat((pd.read_excel(f) for f in all_files))
+        return df
+      except:
+        print("I cannot Concatenate !")
+      
+   def create_Table(self): 
+      #-- Create a new file that contains all Excel data
+      try:
+         df = Preprocess().Concatenate_Table()
+         df.to_excel('Final.xlsx', index=True, header=True)
+      except:
+         print("I cannot create New File !")
+      else:
+         print("Created New File Done.")
 
-all_files = glob.glob("/*.xlsx")
-df = pd.concat((pd.read_excel(f) for f in all_files))
-print("Done!")
-
-df.to_excel('Final.xlsx', index=True, header=True)
-
-print("Done!")
+if __name__ == '__main__':
+    Preprocess().listdir_labels()
