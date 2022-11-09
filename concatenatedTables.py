700a9a339fa88798075bac96dd3f13b03954afbd
Farid Sabahi Moghadam <sabahi.farid@gmail.com>
2022-11-09 20:37:29 +0330

    Commit 1

---
+++ concatenatedTables.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+import os
+import glob
+import pandas as pd
+
+
+entries = os.listdir('/')
+for entry in entries:
+    print(entry)
+
+all_files = glob.glob("/*.xlsx")
+df = pd.concat((pd.read_excel(f) for f in all_files))
+print("Done!")
+
+df.to_excel('Final.xlsx', index=True, header=True)
